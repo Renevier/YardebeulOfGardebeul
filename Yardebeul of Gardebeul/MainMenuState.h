5dbba90f64908f59cba83eb94a5b065a7b791c9b
jeremy_renevier <renevier.jeremy@gmail.com>
2020-11-21 13:31:39 +0100

    bla

--- Yardebeul of Gardebeul/MainMenuState.h
+++ Yardebeul of Gardebeul/MainMenuState.h
@@ -21,7 +21,7 @@ private:
     void InitButton();
 
 public:
-    MainMenuState(RenderWindow* _window, map<string, int>* _supportedKeys, stack<State*>* _states);
+    MainMenuState(RenderWindow* _window, map<string, int>* _supportedKeys, stack<State*>* _states, View* _view);
     virtual void UpdateInput(const float& _dt);
     void UpdateButton();
     virtual void Update(const float& _dt);
